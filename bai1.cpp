87d8cb909e23ef9b41936e3fcf2bd5c6c8c1adae
DUNG KO BIET GI HET <123146672+Dungkobietcode@users.noreply.github.com>
2023-11-22 14:43:21 +0700

    Update bai1.cpp

--- bai1.cpp
+++ bai1.cpp
@@ -1,14 +1,20 @@
 #include <bits/stdc++.h>
+#define ll long long
 using namespace std;
 int main(){
     freopen("bai1.inp","r",stdin);
     freopen("bai1.out","w",stdout);
-    int n,k=0;
+    ll n,k=0;
     cin>>n;
-    for (int i=1;i<=n;i++){
+    int r=sqrt(n);
+    for (int i=1;i<=r;i++){
         if (n%i==0){
-            k++;
+            k+=2;
         }
+    }
+    if(r*r==n){
+        cout<<k-1;
+        return 0;
     }
     cout<<k;
     return 0;
